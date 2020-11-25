5632905c20470246c51dd2d4754956499619035f
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2020-11-25 11:56:18 +0100

    :tada: boostrap

---
+++ src/main/java/fr/publicissapient/ked/webfluxtracing/WebfluxTracingApplication.java
@@ -0,0 +1,13 @@
+package fr.publicissapient.ked.webfluxtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTracingApplication.class, args);
+	}
+
+}
