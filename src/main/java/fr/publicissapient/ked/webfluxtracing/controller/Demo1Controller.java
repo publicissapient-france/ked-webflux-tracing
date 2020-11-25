fff736e76a2fbe257272d3177a880e50c5cd3d1d
Guillaume Barbier <guillaume.barbier2@publicissapient.com>
2020-11-25 17:09:07 +0100

    enabling a controller

--- src/main/java/fr/publicissapient/ked/webfluxtracing/controller/Demo1Controller.java
+++ src/main/java/fr/publicissapient/ked/webfluxtracing/controller/Demo1Controller.java
@@ -1,2 +1,17 @@
-package fr.publicissapient.ked.webfluxtracing.controller;public class Demo1Controller {
+package fr.publicissapient.ked.webfluxtracing.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class Demo1Controller {
+
+
+    @RequestMapping(value = "/test")
+    Mono<ResponseEntity<Object>> accessTest1(ServerWebExchange serverWebExchange) {
+        return Mono.just(ResponseEntity.ok().build());
+    }
 }
