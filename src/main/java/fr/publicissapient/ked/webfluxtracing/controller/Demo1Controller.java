2a569bad299a7ffea78a96f30b1a61225e9bd07f
Guillaume Barbier <guillaume.barbier2@publicissapient.com>
2020-11-25 18:17:48 +0100

    un candidat comme exemple

--- src/main/java/fr/publicissapient/ked/webfluxtracing/controller/Demo1Controller.java
+++ src/main/java/fr/publicissapient/ked/webfluxtracing/controller/Demo1Controller.java
@@ -1,17 +1,48 @@
 package fr.publicissapient.ked.webfluxtracing.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.stream.IntStream;
+
 @RestController
 public class Demo1Controller {
 
+    private final Logger logger;
 
-    @RequestMapping(value = "/test")
-    Mono<ResponseEntity<Object>> accessTest1(ServerWebExchange serverWebExchange) {
-        return Mono.just(ResponseEntity.ok().build());
+    Demo1Controller(Logger logger) {
+
+        this.logger = logger;
+    }
+
+    @RequestMapping(value = "/test",
+    method = RequestMethod.GET)
+    Mono<ResponseEntity<Object>> accessTest1(@RequestParam(name = "id") Integer id, ServerWebExchange serverWebExchange) {
+        try (MDC.MDCCloseable mdcCloseable = MDC.putCloseable("correlationId", id.toString())) {
+            logger.info("Received query for id {}", id);
+            var response = performCount(id)
+            .map(c -> ResponseEntity.ok().build());
+
+            logger.info("TADAM");
+            return response;
+        }
+    }
+
+    private Mono<Integer> performCount(Integer id) {
+        return Mono.just(id)
+                .map(i -> {
+                    var count = IntStream.range(0, 100000)
+                            .boxed()
+                            .reduce(0, Integer::sum);
+                    logger.info("Got count {} for id: {}", count, i);
+                    return count;
+                });
     }
 }
